8c281f18010780ee45bd7fc9d349f865c56d8eb5
Pa7r1Q ForeveR <patro11@hotmail.com>
2020-07-18 22:00:30 +0200

    Added tests and docs

--- README.md
+++ README.md
@@ -1,1 +1,155 @@
-# CoreAPITemplate
+﻿<h1 align="center">
+  <img src="DocsImages/netCoreLogo.png"></a>
+</h1>
+
+
+<h2 align="center">.NET Core API Reference Implementation</h2>
+
+<p align="center">
+  <a href="#about">About</a> ◈
+  <a href="#architecture">Architecture</a> ◈
+  <a href="#prerequisites">Prerequisites</a> ◈
+  <a href="#build-and-run">Build and run</a> ◈
+  <a href="#how-to-test">How to test</a> ◈
+  <a href="#changelog">Changelog</a>
+</p>
+
+## About
+
+An API built on .Net Core 3.1 implementing [Repository &amp; UnitOfWork patterns](https://docs.microsoft.com/en-us/aspnet/mvc/overview/older-versions/getting-started-with-ef-5-using-mvc-4/implementing-the-repository-and-unit-of-work-patterns-in-an-asp-net-mvc-application).
+It includes authentication via [JWT tokens](https://jwt.io/) to access specific resources
+aswell as [MSSQL](https://en.wikipedia.org/wiki/Microsoft_SQL_Server) for data storage 
+with [EntityFramework Core](https://docs.microsoft.com/en-us/ef/core/) as the chosen
+ORM. It also includes [Moq](https://github.com/moq/moq4) to enable near 100% unit test coverage, 
+[Serilog](https://github.com/serilog/serilog) for event logging 
+and [AutoMapper](https://docs.automapper.org/en/stable/Getting-started.html#what-is-automapper) for object mapping.
+
+**The main purpose of the project is to be used as a reference implementation in
+case of need.**
+
+## Architecture
+
+The entire API is composed of multiple layers with the architecture 
+that can be seen on the below image.
+![Architecture](DocsImages/architecture.png)
+
+Highest layer (API) consists of controllers whose job is to authenticate users,
+receive HTTP request and give responses. This layer forwards the requests data
+to the service layer via DI. Service layer contains all the business logic 
+and its main job is processing data. Furthermore, service layer uses 
+UnitOfWork interface obtained through the DI to communicate with the data access layer.
+Data access layers role is to communicate with the database.
+
+The biggest benefit of such architecture
+is that the service layer is completely decoupled from the data access layer.
+Since data access layer is abstracted away from the business logic, we're enabled
+to change the underlying ORM or swap out the entire data access layer with
+minimal changes. Moreover, such architecture enables us to properly test
+our software with unit and integration tests with numerous frameworks and tools
+to achieve as safe and bug-free code as possible.
+
+## Prerequisites
+You will need the following tools:
+
+* [SQL Server Express LocalDB](https://docs.microsoft.com/en-us/sql/database-engine/configure-windows/sql-server-express-localdb?view=sql-server-ver15)
+* [Visual Studio 2019](https://visualstudio.microsoft.com/vs/community/) (version 16.6 or later)
+* [.NET Core SDK 3.1](https://dotnet.microsoft.com/download/dotnet-core/3.1)
+
+## Build and run
+
+You can either run the application through CLI or straight from Visual Studio.
+
+#### Run from CLI
+
+  1. Create a folder on local computer to clone the repo, something like `C:\source` will be fine.
+
+  2. Open cmd within newly created folder and clone the [CarAPITemplate repo from Github](https://github.com/PatriQ94/CarAPITemplate) 
+     ```console
+      git clone https://github.com/PatriQ94/CarAPITemplate.git 
+     ```
+
+  3. Run the application
+     ```console
+       dotnet run --project ./CarAPIRepoPatternTemplate/CarAPIRepoPatternTemplate/Car.API/Car.API.csproj
+     ```
+
+  4. Launch [https://localhost:5001](https://localhost:5001) in your browser to view the API
+
+#### Run from Visual Studio
+
+  1. Create a folder on local computer to clone the repo, something like `C:\source` will be fine.
+
+  2. Open cmd within newly created folder and clone the [CarAPITemplate repo from Github](https://github.com/PatriQ94/CarAPITemplate) 
+     ```console
+      git clone https://github.com/PatriQ94/CarAPITemplate.git 
+     ```
+
+  3. Navigate to ``CarAPIRepoPatternTemplate\CarAPIRepoPatternTemplate`` and 
+open ``CarAPIRepoPatternTemplate.sln`` with Visual Studio.
+
+  4. Once the solution is opened, right click the ``Car.API`` project and
+select ``Set as Startup Project`` if it's not selected already.
+
+![Set As Start Up](DocsImages/SetAsStartUp.png)
+
+  5. The last step is to select ``Car.API`` from the dropdown on top of your screen
+and press the green triangle to run it.
+
+![Run The Program](DocsImages/RunTheProgram.png)
+
+  6. Launch [https://localhost:5001](https://localhost:5001) in your browser to view the API
+   
+## How to test
+
+There is a plethora of ways to test this project and all its functionalities. 
+The two primary ways are through the built-in [Swagger UI](https://swagger.io/tools/swagger-ui/) 
+or through my front-end [VueJS application.](https://github.com/PatriQ94/VueUITemplate)
+
+![Howtotest](DocsImages/howtotest.png)
+
+
+#### Test with Swagger UI
+Swagger UI is an already built-in tool that helps you visualize and interact
+with the API making it very easy to see what the API has to offer. Once you have started the program
+you can visit [https://localhost:5001](https://localhost:5001) in order to see 
+all the endpoints that API offers.
+
+![Swagger](DocsImages/Swagger.png)
+
+It is important to notice that all ``Car`` endpoints require you to be authorized
+in order to access their functionalities. To do so, you need to either register or login. 
+Once that is done, you will receive two JWT tokens: an access token and a refresh
+token. Afterwards, you press the green ``Authorize`` button on the right side, enter 
+your access token with format ``"Bearer <your_token>"`` and click ``Authorize`` like
+shown on the image below.
+
+![J W T](DocsImages/JWT.png)
+
+This will grant you access to Car endpoints until your access token has expired. 
+Then, you will have use the ``/api/Auth/RefreshToken`` endpoint, to gain a newly
+generated access and refresh tokens.
+
+#### Test with the VueJS application
+
+If you would prefer to test this back-end API with a nicer and better looking front-end
+application to simulate a real world scenario feel free to check 
+out [my other project aswell.](https://github.com/PatriQ94/VueUITemplate)
+Below image shows an example of the UI from the VueUITemplate project.
+
+![Garage](DocsImages/Garage.png)
+
+### Changelog
+
+ Edited |Comment |
+------ |------ |
+5.6.2020 | Project creation, added swagger, mapped out initial structure |
+8.6.2020 | Added database connection and migrations |
+9.6.2020 | Improved API documentation on swagger |
+11.6.2020 | Added login and register endpoints, code refactoring |
+12.6.2020 | Refactored most of the project, changed database entities, added refresh tokens endpoint  
+15.6.2020 | Refactoring, code cleanup and improved documentation 
+16.6.2020 | Created .Tests project, 100% test coverage of car service
+18.6.2020 | Added Serilog for logging
+19.6.2020 | Added AutoMapper for object mapping
+8.7.2020 and on | Improvements of the documentation
+18.7.2020 and on | Restructured the entire repository
---
+++ Tests/CarServiceTests.cs
@@ -0,0 +1,232 @@
+﻿using Models;
+using Services.Services;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Tests
+{
+    public class CarServiceTests
+    {
+        private readonly CarService _carService;
+        private readonly Mock<IUnitOfWork> _unitOfWorkMock = new Mock<IUnitOfWork>();
+
+        public CarServiceTests()
+        {
+            _carService = new CarService(_unitOfWorkMock.Object);
+        }
+
+        [Fact]
+        public async Task InsertAsync_ShouldReturnCarId()
+        {
+            //Arrange
+            string userId = Guid.NewGuid().ToString();
+            string brand = "Volkswagen";
+            string model = "Golf 5";
+            int year = 2020;
+            int kilometers = 5000;
+            long expected = 3;
+            _unitOfWorkMock.Setup(x => x.Car.InsertAsync(userId, brand, model, year, kilometers)).ReturnsAsync(expected);
+
+            //Act
+            var actual = await _carService.InsertAsync(userId, brand, model, year, kilometers);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public async Task InsertAsync_ShouldThrowException_WhenInputParametersAreInCorrect()
+        {
+            //Arrange
+            string userId = Guid.NewGuid().ToString();
+            string brand = "Volkswagen";
+            string model = "Golf 5";
+            int year = 2020;
+            int kilometers = 5000;
+            long carId = 3;
+            string expected = "Incorrect input parameters, please try again, try again...";
+            _unitOfWorkMock.Setup(x => x.Car.InsertAsync(userId, brand, model, year, kilometers)).ReturnsAsync(carId);
+
+            //Act
+            Task actual() => _carService.InsertAsync("", "", "", year, kilometers);
+
+            //Assert
+            Exception ex = await Assert.ThrowsAsync<Exception>(actual);
+            Assert.Equal(expected, ex.Message);
+        }
+
+        [Fact]
+        public async Task GetByUserAsync_ShouldReturnOneCar()
+        {
+            //Arrange
+            var userId = Guid.NewGuid().ToString();
+            List<Models.Domain.Car> userCars = new List<Models.Domain.Car>()
+            {
+                new Models.Domain.Car(){
+                    UserId = userId,
+                    ID = 1,
+                    Brand = "Volkswagen",
+                    Kilometers = 5000,
+                    Year = 2020,
+                    Model = "Golf 5"
+                }
+            };
+            _unitOfWorkMock.Setup(x => x.Car.GetByUserAsync(userId)).ReturnsAsync(userCars);
+
+            //Act
+            var actual = await _carService.GetByUserAsync(userId);
+
+            //Assert
+            Assert.Single(actual);
+            Assert.Equal(userId, actual[0].UserId);
+            Assert.Equal("Golf 5",actual[0].Model);
+        }
+
+        [Fact]
+        public async Task GetByUserAsync_ShouldReturnEmpty_WhenUserDoesntExistOrHasNoCars()
+        {
+            //Arrange
+            _unitOfWorkMock.Setup(x => x.Car.GetByUserAsync(It.IsAny<string>())).ReturnsAsync(new List<Models.Domain.Car>());
+
+            //Act
+            var actual = await _carService.GetByUserAsync(Guid.NewGuid().ToString());
+
+            //Assert
+            Assert.Empty(actual);
+        }
+
+        [Fact]
+        public async Task RemoveAsync_ShouldReturnSuccessfulString()
+        {
+            //Arrange
+            var userId = Guid.NewGuid().ToString();
+            int carId = 3;
+            string expected = "Successfully removed!";
+            Models.Domain.Car userHasCar = new Models.Domain.Car()
+            {
+                ID = carId
+            };
+            _unitOfWorkMock.Setup(x => x.Car.CheckIfUserHasCarAsync(userId, carId)).ReturnsAsync(userHasCar);
+            _unitOfWorkMock.Setup(x => x.Car.RemoveAsync(userHasCar.ID)).ReturnsAsync(true);
+
+            //Act
+            var actual = await _carService.RemoveAsync(userId, carId);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public async Task RemoveAsync_ShouldThrowException_WhenUserDoesntHaveCarByID()
+        {
+            //Arrange
+            var userId = Guid.NewGuid().ToString();
+            int carId = 3;
+            string expected = $"User does not have a car with the provided ID";
+            Models.Domain.Car userHasCar = new Models.Domain.Car()
+            {
+                ID = carId
+            };
+            _unitOfWorkMock.Setup(x => x.Car.CheckIfUserHasCarAsync(userId, carId)).ReturnsAsync(() => null);
+
+            //Act
+            Task actual() => _carService.RemoveAsync(userId, carId);
+
+            //Assert
+            Exception ex = await Assert.ThrowsAsync<Exception>(actual);
+            Assert.Equal(expected, ex.Message);
+        }
+
+        [Fact]
+        public async Task RemoveAsync_ShouldThrowException_WhenRemovingFromDatabaseFails()
+        {
+            //Arrange
+            var userId = Guid.NewGuid().ToString();
+            int carId = 3;
+            string expected = $"Couldn't remove from database";
+            Models.Domain.Car userHasCar = new Models.Domain.Car()
+            {
+                ID = carId
+            };
+            _unitOfWorkMock.Setup(x => x.Car.CheckIfUserHasCarAsync(userId, carId)).ReturnsAsync(userHasCar);
+            _unitOfWorkMock.Setup(x => x.Car.RemoveAsync(userHasCar.ID)).ReturnsAsync(false);
+
+            //Act
+            Task actual() => _carService.RemoveAsync(userId, carId);
+
+            //Assert
+            Exception ex = await Assert.ThrowsAsync<Exception>(actual);
+            Assert.Equal(expected, ex.Message);
+        }
+
+        [Fact]
+        public async Task Update_KilometersAsync_ShouldReturnSuccessfulString()
+        {
+            //Arrange
+            var userId = Guid.NewGuid().ToString();
+            int carId = 3;
+            int kilometers = 5000;
+            string expected = "Successfully updated!";
+            Models.Domain.Car userHasCar = new Models.Domain.Car()
+            {
+                ID = carId
+            };
+            _unitOfWorkMock.Setup(x => x.Car.CheckIfUserHasCarAsync(userId, carId)).ReturnsAsync(userHasCar);
+            _unitOfWorkMock.Setup(x => x.Car.UpdateCarKilometersAsync(userHasCar.ID, kilometers)).ReturnsAsync(true);
+
+            //Act
+            var actual = await _carService.Update_KilometersAsync(userId, userHasCar.ID, kilometers);
+
+            //Assert
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public async Task Update_KilometersAsync_ShouldThrowException_WhenUserDoesntHaveCarByID()
+        {
+            //Arrange
+            var userId = Guid.NewGuid().ToString();
+            int carId = 3;
+            int kilometers = 5000;
+            string expected = $"User does not have a car with the provided ID";
+            Models.Domain.Car userHasCar = new Models.Domain.Car()
+            {
+                ID = carId
+            };
+            _unitOfWorkMock.Setup(x => x.Car.CheckIfUserHasCarAsync(userId, carId)).ReturnsAsync(() => null);
+
+            //Act
+            Task actual() => _carService.Update_KilometersAsync(userId, userHasCar.ID, kilometers);
+
+            //Assert
+            Exception ex = await Assert.ThrowsAsync<Exception>(actual);
+            Assert.Equal(expected, ex.Message);
+        }
+
+        [Fact]
+        public async Task Update_KilometersAsync_ShouldThrowException_WhenUpdatingKilometersFails()
+        {
+            //Arrange
+            var userId = Guid.NewGuid().ToString();
+            int carId = 3;
+            string expected = $"Couldn't update the record";
+            int kilometers = 5000;
+            Models.Domain.Car userHasCar = new Models.Domain.Car()
+            {
+                ID = carId
+            };
+            _unitOfWorkMock.Setup(x => x.Car.CheckIfUserHasCarAsync(userId, carId)).ReturnsAsync(userHasCar);
+            _unitOfWorkMock.Setup(x => x.Car.UpdateCarKilometersAsync(userHasCar.ID, kilometers)).ReturnsAsync(false);
+
+            //Act
+            Task actual() => _carService.Update_KilometersAsync(userId, userHasCar.ID, kilometers);
+
+            //Assert
+            Exception ex = await Assert.ThrowsAsync<Exception>(actual);
+            Assert.Equal(expected, ex.Message);
+        }
+    }
+}
